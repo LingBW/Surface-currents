08440aff1455550d317eaf6f235375608b8a5cea
Bingwei Ling <ufwrgw@gmail.com>
2016-11-16 15:26:15 -0500

    Update and rename surface_Currents_M_functions100401.py to surface_Currents_M_functions.py

--- surface_Currents_M_functions.py
+++ surface_Currents_M_functions.py
@@ -125,28 +125,27 @@ class get_fvcom():
                 raise Exception()
             npTimes = np.array(Times)
             tm1 = npTimes-starttime; #tm2 = mtime-t2
-            index1 = np.argmin(abs(tm1))#'''
+            self.index1 = np.argmin(abs(tm1))#'''
             #index1 = netCDF4.date2index(starttime,MTime,select='nearest')
-            index2 = index1 + self.hours#'''
+            self.index2 = self.index1 + self.hours#'''
             #print 'index1,index2',index1,index2
             #url = url.format(index1, index2)
-            self.mTime = Times[index1:index2+1]
+            self.mTime = Times[self.index1:self.index2+1]
             
             self.url = turl
             
         elif self.modelname == "massbay":
-            timeurl = '''http://www.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NECOFS_FVCOM_OCEAN_MASSBAY_FORECAST.nc?Times[0:1:144]'''
-            url = """http://www.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NECOFS_FVCOM_OCEAN_MASSBAY_FORECAST.nc?
-            lon[0:1:98431],lat[0:1:98431],lonc[0:1:165094],latc[0:1:165094],siglay[0:1:9][0:1:98431],h[0:1:98431],
-            nbe[0:1:2][0:1:165094],u[{0}:1:{1}][0:1:9][0:1:165094],v[{0}:1:{1}][0:1:9][0:1:165094],zeta[{0}:1:{1}][0:1:98431]"""
+            turl = '''http://www.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NECOFS_FVCOM_OCEAN_MASSBAY_FORECAST.nc?'''
             
             try:
-                mTime = netCDF4.Dataset(timeurl).variables['Times'][:]              
+                self.tdata = netCDF4.Dataset(turl).variables
+                #MTime = tdata['time'] 
+                MTime = self.tdata['Times']              
             except:
                 print '"massbay" database is unavailable!'
                 raise Exception()
             Times = []
-            for i in mTime:
+            for i in MTime:
                 strt = '201'+i[3]+'-'+i[5]+i[6]+'-'+i[8]+i[9]+' '+i[11]+i[12]+':'+i[14]+i[15]
                 Times.append(datetime.strptime(strt,'%Y-%m-%d %H:%M'))
             fmodtime = Times[0]; emodtime = Times[-1]         
@@ -157,10 +156,10 @@ class get_fvcom():
             tm1 = npTimes-starttime; #tm2 = mtime-t2
             index1 = np.argmin(abs(tm1))
             index2 = index1 + self.hours#'''
-            url = url.format(index1, index2)
+            #url = url.format(index1, index2)
             self.mTime = Times[index1:index2+1]
             
-            self.url = url
+            self.url = turl
 
         elif self.modelname == "30yr": #start at 1977/12/31 23:00, end at 2014/1/1 0:0, time units:hours
             timeurl = """http://www.smast.umassd.edu:8080/thredds/dodsC/fvcom/hindcasts/30yr_gom3?time[0:1:316008]"""
@@ -206,8 +205,8 @@ class get_fvcom():
         self.lonc, self.latc = self.tdata['lonc'][:], self.tdata['latc'][:]  #quantity:165095
         #self.lons, self.lats = self.data['lon'][:], self.data['lat'][:]
         #self.h = self.data['h'][:]; self.siglay = self.data['siglay'][:]; #nv = self.data['nv'][:]
-        self.u = self.tdata['u'][:,0,:][:]; self.v = self.tdata['v'][:,0,:][:]#; self.zeta = self.data['zeta']
-        
+        self.u = self.tdata['u'][self.index1:self.index2,0,:][:]; self.v = self.tdata['v'][self.index1:self.index2,0,:][:]#; self.zeta = self.data['zeta']
+        print 'model length',len(self.u),len(self.v)
         nbe1=self.tdata['nbe'][0];nbe2=self.tdata['nbe'][1];
         nbe3=self.tdata['nbe'][2]
         pointt = np.vstack((nbe1,nbe2,nbe3)).T; self.pointt = pointt
@@ -314,14 +313,14 @@ class get_fvcom():
                 pa.append((tlons[i],tlats[i]))
             #path = Path(pa)#,codes
             return pa
-    def current_track(self,jn): #,point,leh):
+    def current_track(self,jn,track_way): #,point,leh):
         cts = []        
         #self.lonk,self.latk = self.shrink_data(lon,lat,self.lons,self.lats,leh)
-        epoints = np.vstack((self.lonl,self.latl)).T
+        epoints = np.vstack((self.lonl[::3],self.latl[::3])).T
         numep = len(epoints)
         for i in range(numep):
-            print '%d of %d, %d' % (i+1,numep,jn+1)
-            getk,pnu = self.get_track(jn,epoints[i][0],epoints[i][1])
+            #print '%d of %d, %d' % (i+1,numep,jn+1)
+            getk = self.get_track(jn,epoints[i][0],epoints[i][1],track_way)
             #print type(getk['lon']),type(getk['lat']),type(getk['layer']),type(getk['spd'])
             ld = min(len(getk['lon']),len(getk['lat']),len(getk['spd']))
             for j in getk:
@@ -334,7 +333,7 @@ class get_fvcom():
             cts.append(pgetk)
         return cts
         
-    def get_track(self,jnu,lon,lat): #,b_index,nvdepth,,bcon 
+    def get_track(self,jnu,lon,lat,track_way): #,b_index,nvdepth,,bcon 
         '''
         Get forecast points start at lon,lat
         '''
@@ -352,7 +351,7 @@ class get_fvcom():
             elementindex = np.intersect1d(index1,index2)
             
         except:
-            return modpts,0  
+            return modpts#,0  
             
         t = abs(self.hours) 
     
@@ -362,8 +361,13 @@ class get_fvcom():
                 lonl,latl = self.shrink_data(lon,lat,self.lonc,self.latc,0.5)
                 lonk,latk = self.shrink_data(lon,lat,self.lons,self.lats,0.5)#'''
             if i<jnu: continue
-            modpts['time'].append(self.mTime[i])
-            u_t1 = self.u[i,elementindex][0]; v_t1 = self.v[i,elementindex][0]
+            
+            if track_way=='backward':
+                u_t1 = -self.u[t-1-i,elementindex][0]; v_t1 = -self.v[t-1-i,elementindex][0]
+                modpts['time'].append(self.mTime[t-1-i])
+            else:
+                u_t1 = self.u[i,elementindex][0]; v_t1 = self.v[i,elementindex][0]
+                modpts['time'].append(self.mTime[i])
                 #u_t2 = self.u[i+1,layer,elementindex][0]; v_t2 = self.v[i+1,layer,elementindex][0]
             #u_t,v_t = self.uvt(u_t1,v_t1,u_t2,v_t2)
             #u_t = (u_t1+u_t2)/2; v_t = (v_t1+v_t2)/2
@@ -372,9 +376,84 @@ class get_fvcom():
             pspeed = math.sqrt(u_t1**2+v_t1**2)
             modpts['spd'].append(pspeed)
             if i == t-1:# stop when got the last point speed.
-                return modpts,2
+                return modpts#,2
             if i >= jnu+4:# break
-                return modpts,2
+                return modpts#,2
+            #x,y = mapx(lon,lat)
+            #temlon,temlat = mapx(x+dx,y+dy,inverse=True)            
+            temlon = lon + (dx/(111111*np.cos(lat*np.pi/180)))
+            temlat = lat + dy/111111 #'''
+            
+            #########################
+            lon = temlon; lat = temlat
+            modpts['lon'].append(lon); modpts['lat'].append(lat)#; modpts['layer'].append(layer); 
+            #print '%d,Lat,Lon,Speed'%(i+1),temlat,temlon,pspeed              
+            try:
+                if self.modelname == "GOM3" or self.modelname == "30yr":
+                    lonp,latp = self.nearest_point(lon, lat, self.lonl, self.latl,0.2)
+                    #lonn,latn = self.nearest_point(lon,lat,self.lonk,self.latk,0.3)
+                if self.modelname == "massbay":
+                    lonp,latp = self.nearest_point(lon, lat, self.lonl, self.latl,0.03)
+                    #lonn,latn = self.nearest_point(lon,lat,self.lonk,self.latk,0.05)
+                index1 = np.where(self.lonc==lonp)
+                index2 = np.where(self.latc==latp)
+                elementindex = np.intersect1d(index1,index2); #print 'elementindex',elementindex
+                #index3 = np.where(self.lons==lonn)
+                #index4 = np.where(self.lats==latn)
+                #nodeindex = np.intersect1d(index3,index4)
+                if elementindex in self.inde[0]:
+                    #print 'boundary elementindex',elementindex,type(elementindex)
+                    return modpts#,2  # hits the boundary.
+            except:
+                return modpts#,1
+    def get_dtrack(self,dtime,lon,lat,track_way): #,b_index,nvdepth,,bcon 
+        '''
+        Get forecast points start at lon,lat
+        '''
+        modpts = dict(lon=[lon], lat=[lat], time=[], spd=[]) #model forecast points, layer=[]
+        tm1 = np.array(self.mTime)-dtime
+        jnu = np.argmin(abs(tm1))
+        try:
+            if self.modelname == "GOM3" or self.modelname == "30yr":
+                lonp,latp = self.nearest_point(lon, lat, self.lonl, self.latl,0.2)
+                #lonn,latn = self.nearest_point(lon,lat,self.lonk,self.latk,0.3)
+            if self.modelname == "massbay":
+                lonp,latp = self.nearest_point(lon, lat, self.lonl, self.latl,0.03)
+                #lonn,latn = self.nearest_point(lon,lat,self.lonk,self.latk,0.05)        
+            index1 = np.where(self.lonc==lonp)
+            index2 = np.where(self.latc==latp)
+            elementindex = np.intersect1d(index1,index2)
+            
+        except:
+            return modpts#,0  
+            
+        t = abs(self.hours) 
+    
+        for i in xrange(t):            
+            '''if i!=0 and i%24==0 :
+                #print 'layer,lon,lat,i',layer,lon,lat,i
+                lonl,latl = self.shrink_data(lon,lat,self.lonc,self.latc,0.5)
+                lonk,latk = self.shrink_data(lon,lat,self.lons,self.lats,0.5)#'''
+            if i<jnu: continue
+            #modpts['time'].append(self.mTime[i])
+            #u_t1 = self.u[i,elementindex][0]; v_t1 = self.v[i,elementindex][0]
+            if track_way=='backward':
+                u_t1 = -self.u[t-1-i,elementindex][0]; v_t1 = -self.v[t-1-i,elementindex][0]
+                modpts['time'].append(self.mTime[t-1-i])
+            else:
+                u_t1 = self.u[i,elementindex][0]; v_t1 = self.v[i,elementindex][0]
+                modpts['time'].append(self.mTime[i])
+                #u_t2 = self.u[i+1,layer,elementindex][0]; v_t2 = self.v[i+1,layer,elementindex][0]
+            #u_t,v_t = self.uvt(u_t1,v_t1,u_t2,v_t2)
+            #u_t = (u_t1+u_t2)/2; v_t = (v_t1+v_t2)/2
+            
+            dx = 60*60*u_t1; dy = 60*60*v_t1
+            pspeed = math.sqrt(u_t1**2+v_t1**2)
+            modpts['spd'].append(pspeed)
+            if i == t-1:# stop when got the last point speed.
+                return modpts#,2
+            #if i >= jnu+4:# break
+                #return modpts#,2
             #x,y = mapx(lon,lat)
             #temlon,temlat = mapx(x+dx,y+dy,inverse=True)            
             temlon = lon + (dx/(111111*np.cos(lat*np.pi/180)))
@@ -399,10 +478,9 @@ class get_fvcom():
                 #nodeindex = np.intersect1d(index3,index4)
                 if elementindex in self.inde[0]:
                     print 'boundary elementindex',elementindex,type(elementindex)
-                    return modpts,2  # hits the boundary.
+                    return modpts#,2  # hits the boundary.
             except:
-                return modpts,1
-                                
+                return modpts#,1                              
         #return modpts,2
 def draw_basemap(ax, points, interval_lon=0.5, interval_lat=0.5):
     '''
@@ -431,3 +509,96 @@ def draw_basemap(ax, points, interval_lon=0.5, interval_lat=0.5):
     #dmap.fillcontinents(color='grey')
     dmap.drawmapboundary()
     #dmap.etopo()
+    
+class get_drifter():
+
+    def __init__(self, drifter_id, filename=None):
+        self.drifter_id = drifter_id
+        self.filename = filename
+        
+    def getrawdrift(self, did,filename):
+        
+        '''
+        routine to get raw drifter data from ascii files posted on the web
+        '''
+        url='http://nefsc.noaa.gov/drifter/'+filename
+        df=pd.read_csv(url,header=None, delimiter="\s+")
+        # make a datetime
+        dtime=[]
+        index = np.where(df[0]==int(did))[0]
+        newData = df.ix[index]
+        for k in newData[0].index:
+            dt1=datetime(2016, newData[2][k],newData[3][k],newData[4][k],newData[5][k])
+            dtime.append(dt1)
+        #print dtime
+        return newData[8],newData[7],dtime,newData[9] # lat,lon,time,
+
+    def getdrift(self,did):
+        
+        """
+        routine to get drifter data from archive based on drifter id (did)
+        -assumes "import pandas as pd" has been issued above
+        -get remotely-stored drifter data via ERDDAP
+        -input: deployment id ("did") number where "did" is a string
+        -output: time(datetime), lat (decimal degrees), lon (decimal degrees), depth (meters)
+        
+        note: there is another function below called "data_extracted" that does a similar thing returning a dictionary
+        
+        Jim Manning June 2014
+        """
+        url = 'http://comet.nefsc.noaa.gov:8080/erddap/tabledap/drifters.csv?time,latitude,longitude,depth&id="'+did+'"&orderBy("time")'
+        df=pd.read_csv(url,skiprows=[1]) #returns a dataframe with all that requested
+        #print df    
+        # generate this datetime 
+        for k in range(len(df)):
+           df.time[k]=parse(df.time[k]) # note this "parse" routine magically converts ERDDAP time to Python datetime
+        return df.latitude.values,df.longitude.values,df.time.values,df.depth.values 
+        
+    def get_track(self, starttime=None, days=None):
+        '''
+        return drifter nodes
+        if starttime is given, return nodes started from starttime
+        if both starttime and days are given, return nodes of the specific time period
+        '''
+        if self.filename:
+            temp = self.getrawdrift(self.drifter_id,self.filename)
+        else:
+            temp = self.getdrift(self.drifter_id)
+        nodes = {}
+        nodes['lon'] = np.array(temp[1])
+        nodes['lat'] = np.array(temp[0])
+        nodes['time'] = np.array(temp[2])
+        #starttime = np.array(temp[2][0])
+        if not starttime:
+            starttime = np.array(temp[2][0])
+        if days:
+            endtime = starttime + timedelta(days=days)
+            i = self.__cmptime(starttime, nodes['time'])
+            j = self.__cmptime(endtime, nodes['time'])
+            
+            if i == j :
+                print 'No data in given duration.'
+                raise Exception()
+                #return nodes
+            nodes['lon'] = nodes['lon'][i:j+1]
+            nodes['lat'] = nodes['lat'][i:j+1]
+            nodes['time'] = nodes['time'][i:j+1]
+        else:
+            i = self.__cmptime(starttime, nodes['time'])
+            nodes['lon'] = nodes['lon'][i:-1]
+            nodes['lat'] = nodes['lat'][i:-1]
+            nodes['time'] = nodes['time'][i:-1]
+        return nodes
+        
+    def __cmptime(self, time, times):
+        '''
+        return indies of specific or nearest time in times.
+        '''
+        tdelta = []
+        #print len(times)
+        for t in times:
+            tdelta.append(abs((time-t).total_seconds()))
+            
+        index = tdelta.index(min(tdelta))
+        
+        return index
