dc8aa77cc68cb617395e0a948d08f59856f023bc
Bingwei Ling <ufwrgw@gmail.com>
2018-01-23 15:29:41 -0500

    Update surface_Currents-M.py

--- surface_Currents-M.py
+++ surface_Currents-M.py
@@ -27,14 +27,8 @@ import matplotlib as mpl
 from pydap.client import open_url
 
 st_run_time = datetime.now() # Caculate execution time with en_run_time
-############################### Options #######################################
-'''
-Option 1: Drifter track.
-Option 2: Specify the start point.
-Option 3: Specify the start point with simulated map.
-Option 4: Area(box) track.          
-'''
-######## Hard codes ##########
+         
+########################## Hard codes ############################################
 MODEL = 'GOM3'     # 'ROMS', 'GOM3','massbay','30yr'
 GRIDS = ['GOM3','massbay','30yr']    # All belong to FVCOM. '30yr' works from 1977/12/31 22:58 to 2014/1/1 0:0
 #depth = 1    # depth below ocean surface, positive
@@ -55,8 +49,6 @@ save_dir = './Results/'
 #colors = ['magenta','cyan','olive','blue','orange','green','red','yellow','black','purple']
 #'#0039b3','#0049e6','#1a62ff','#4d85ff','#80a8ff',,'#b3cbff','#e6eeff'
 #colors = ['#99ffff','#b3ffff','#ccffff','#e6ffff','#ffffff','#ffebe6','#ffc2b3','#ff9980','#ff704d','#ff471a','#e62e00','#b32400'] #16
-colors = ['#00bfff','#00ffff','#00ffbf','#00ff80','#80ff00','#bfff00','#ffff00','#ffbf00','#ff8000','#ff4000']
-cmap = mpl.colors.ListedColormap(colors)
 '''
 utcti = datetime.utcnow(); utct = utcti.strftime('%H')
 locti = datetime.now(); loct = locti.strftime('%H')
@@ -65,42 +57,44 @@ if ditnu < 0:
     ditnu = int(utct)+24-int(loct)
 locstart_time = start_time - timedelta(hours=ditnu)#'''
 
-################################## Option ####################################
-centerpoint = (41.91,-70.233)
-#centerpoint = (41.977,-70.375)
-bordersidele = 0.2
-#lats = get_obj.points_square(centerpoint,0.1)
 ############################## Drifter parts ##################################
-drifterIDs = ['160410706']#,'1604107014''160410704',,'160410707'
+drifterIDs = ['176440663']#,'1604107014''160410704',,'160410707'
 clrs = ['b','g','m','y','c']
 INPUT_DATA = 'drift_X.dat'
 track_days = 2 #the days of real drifter track.
 dstart_time = datetime.utcnow()-timedelta(track_days)
 
-drifter_points = {} #dict(lon=[],lat=[])
 print 'Get drifter_points'
+drifter_points = {} #dict(lon=[],lat=[])
 start_times = []
+cenlat,cenlon = [],[]
 for i in drifterIDs:
     dps = []; 
     drifter = get_drifter(i, INPUT_DATA)
     dr_points = drifter.get_track(dstart_time,track_days)
     dps.append(dr_points['lon']); dps.append(dr_points['lat'])
     start_times.append(dr_points['time'][-1])
+    cenlon.append(dr_points['lon'][-1]); cenlat.append(dr_points['lat'][-1]);
     drifter_points[i]=dps
+    
 start_time = min(start_times) # datetime.utcnow()
+#print 'start_time',start_time
 end_time = start_time + timedelta(hours=track_hours)
 
-
-############################## Common codes ###################################
-
+#print "cenlat,cenlon,np.mean(cenlat),np.mean(cenlon)",cenlat,cenlon,'n/',np.mean(cenlat),np.mean(cenlon)
+centerpoint = (np.mean(cenlat),np.mean(cenlon))
+bordersidele = 0.3
+#centerpoint = (41.91,-70.233)
+#bordersidele = 0.22
+############################## model codes ###################################
+print 'Get model_points'
 points = {'lats':[],'lons':[]}  # collect all points we've gained   
-
+model_points = {};
 get_obj = get_fvcom(MODEL)
 toltime = get_obj.get_url(start_time,end_time)
 b_points,psqus = get_obj.get_data(centerpoint,bordersidele)# b_points is model boundary points.
 points['lons'].extend(psqus[1]);points['lats'].extend(psqus[0])
-model_points = {};
-print 'Get model_points'
+
 for j in range(len(drifterIDs)):
     dlon = drifter_points[drifterIDs[j]][0][-1]
     dlat = drifter_points[drifterIDs[j]][1][-1]
@@ -111,10 +105,9 @@ currents_points = {}
 print 'Get currents_points'
 for j in range(track_hours):#len(toltime)
     #if j==0 or (j+1)%2==0 :
-    if j%3==0:
-        print j
-        cpoints = get_obj.current_track(j,track_way)
-        currents_points[toltime[j]] = cpoints#'''
+    #if j%6==0:
+    cpoints = get_obj.current_track(j,track_way)
+    currents_points[toltime[j]] = cpoints#'''
  
    
 '''try:
@@ -130,19 +123,9 @@ fig = plt.figure() #figsize=(16,9)
 ax = fig.add_subplot(111)
 
 crang = np.linspace(0,2.0, num=10) #color range
-norm = mpl.colors.Normalize(vmin=0, vmax=2.0)
-#minspd = np.argmin(crang-speed)
-#ax.plot(points['lons'],points['lats'])
-#ax.plot(psqus[1],psqus[0])
-draw_basemap(ax, points)  # points is using here
-#ax.plot(model_points['1604107018']['lon'],model_points['1604107018']['lat'],'r-')
-#ax.plot(drifter_points['1604107018'][0],drifter_points['1604107018'][1],'b-')
-
-levels=np.arange(-80,0,10)
+########################### get depth contour data####################################
+'''levels=np.arange(-80,0,10)
 ss=1
-cont_range = [-3,0]
-draw_parallels = 'ON'
-
 url='http://geoport.whoi.edu/thredds/dodsC/bathy/gom03_v1_0'
 dataset=open_url(url)
 basemap_lat=dataset['lat']
@@ -161,14 +144,21 @@ max_index_lat=max(index_minlat,index_maxlat)
 min_index_lon=min(index_minlon,index_maxlon)
 max_index_lon=max(index_minlon,index_maxlon)
 X,Y=np.meshgrid(basemap_lon[min_index_lon:max_index_lon:ss],basemap_lat[min_index_lat:max_index_lat:ss])
+#'''
 
-
+####################### setting scale#############################################
+colors = ['#00bfff','#00ffff','#00ffbf','#00ff80','#80ff00','#bfff00','#ffff00','#ffbf00','#ff8000','#ff4000']
+cont_range = [-3,0]
+draw_parallels = 'ON'
+cmap = mpl.colors.ListedColormap(colors)
+norm = mpl.colors.Normalize(vmin=0, vmax=2.0)
 cax = fig.add_axes([0.85, 0.2, 0.02, 0.6])                    
 cb1 = mpl.colorbar.ColorbarBase(cax, cmap=cmap,norm=norm,spacing='proportional',orientation='vertical')#,orientation='horizontal')
-cb1.set_label('M/S')
+cb1.set_label('M/S')#'''
 
 lminlat0 = min(points['lats'])+0.01;lmaxlon0 = max(points['lons'])-0.07
-lminlat1 = lminlat0; lmaxlon1 = lmaxlon0+3600*0.44704/(111111*np.cos(lminlat0*np.pi/180))
+lminlat1 = lminlat0; lmaxlon1 = lmaxlon0+1609.344/(111111*np.cos(lminlat0*np.pi/180)) # 1 mile = 1609.344 meter'''
+
 def animate(n): #del ax.collections[:]; del ax.lines[:]; ax.cla(); ax.lines.remove(line)        
     '''if track_way=='backward':
         Time = (locstart_time-timedelta(hours=n)).strftime("%d-%b-%Y %H:%M")
@@ -177,6 +167,8 @@ def animate(n): #del ax.collections[:]; del ax.lines[:]; ax.cla(); ax.lines.remo
     #plt.suptitle('%.f%% simulated drifters ashore\n%d days, %d m, %s'%(int(round(p)),track_days,depth,Time))
     #plt.suptitle('Current model %d' % n)
     ax.cla()
+    #del ax.collections[:]
+    draw_basemap(ax, points)  # points is using here
     plt.suptitle('FVCOM Surface Currents \n' + toltime[n].strftime("%d-%b-%Y %H:%M"))
     #del ax.lines[:] 
     for i in range(len(drifterIDs)):
@@ -188,25 +180,29 @@ def animate(n): #del ax.collections[:]; del ax.lines[:]; ax.cla(); ax.lines.remo
             du = model_points[drifterIDs[i]]['time'].index(toltime[n])
             ax.plot(model_points[drifterIDs[i]]['lon'][:du+1],model_points[drifterIDs[i]]['lat'][:du+1],'-',color='r',lw=3.)
         if toltime[n] > model_points[drifterIDs[i]]['time'][-1]:
-            ax.plot(model_points[drifterIDs[i]]['lon'],model_points[drifterIDs[i]]['lat'],'-',color='r',lw=3.)
+            ax.plot(model_points[drifterIDs[i]]['lon'],model_points[drifterIDs[i]]['lat'],'-',color='r',lw=3.)#'''
+    '''# plot depth contour###########################
     CS=ax.contour(X,Y,basemap_topo.topo[min_index_lat:max_index_lat:ss,index_minlon:index_maxlon:ss],levels,cmap=plt.cm.gist_earth,linewidths=0.5)
     ax.clabel(CS,fmt='%5.0f')#, np.arange(-80,0,20), inline=1)fontsize=3,
-    ax.contourf(X,Y,basemap_topo.topo[min_index_lat:max_index_lat:ss,min_index_lon:max_index_lon:ss],[0,1000],colors='gray')
+    ax.contourf(X,Y,basemap_topo.topo[min_index_lat:max_index_lat:ss,min_index_lon:max_index_lon:ss],[0,1000],colors='gray')#'''
+    
     for k in currents_points:
         kl = currents_points[k]
         for j in xrange(len(kl)):
             if any(kl[j]['time']==toltime[n]):
                 nu = kl[j]['time'][kl[j]['time']==toltime[n]].index[0]
                 if nu < len(kl[j]['lon'])-1 :    # if Index less than length of list, plot index and index+1 
-                #if nu>=1:
                     speed = kl[j]['spd'][nu]
                     cl=colors[np.argmin(abs(crang-speed))]
-                    #if n<len(lon_set[j]):
-                    #ax.plot(kl[j]['lon'][nu:nu+2],kl[j]['lat'][nu:nu+2],'-',color=colors[np.argmin(abs(crang-speed))])
                     dx=kl[j]['lon'][nu+1]-kl[j]['lon'][nu]; dy=kl[j]['lat'][nu+1]-kl[j]['lat'][nu]
-                    ax.arrow(kl[j]['lon'][nu],kl[j]['lat'][nu],dx,dy,fc=cl, ec=cl,head_width=0.005, head_length=0.01)
+                    ax.arrow(kl[j]['lon'][nu],kl[j]['lat'][nu],dx,dy,fc=cl, ec=cl,head_width=0.005, head_length=0.01)#'''
+                    #ax.plot(kl[j]['lon'][nu:nu+2],kl[j]['lat'][nu:nu+2],'-',color=colors[np.argmin(abs(crang-speed))])
+                    '''dx=kl[j]['lon'][nu+1]-kl[j]['lon'][nu]; dy=kl[j]['lat'][nu+1]-kl[j]['lat'][nu]; m = np.hypot(dx,dy)
+                    Q=ax.quiver(kl[j]['lon'][nu],kl[j]['lat'][nu],dx,dy,m,scale_units='xy', angles='xy', scale=1)#'''
+                    #plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{mile}{h}$', labelpos='E',coordinates='figure')
+    # plot the scale
     ax.plot([lmaxlon0,lmaxlon1],[lminlat0,lminlat1],'-|',color='k',linewidth=3)
-    ax.text(lmaxlon1,lminlat1+0.004,'1 mph')
+    ax.text(lmaxlon1,lminlat1+0.004,'1 mph')#'''
 anim = animation.FuncAnimation(fig, animate, frames=track_hours,interval=500) #,
 en_run_time = datetime.now()
 print 'Take '+str(en_run_time-st_run_time)+' running the code.\nStart at '+str(st_run_time)+'\nEnd at   '+str(en_run_time)
